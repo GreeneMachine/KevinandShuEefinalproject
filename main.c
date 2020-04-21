3c517ac40c322d92ffae66bf5c02b30ebe9d3d01
ongsh <ongsh@192.168.0.13>
2020-04-21 10:33:34 -0500

    shuee's commit

--- main.c
+++ main.c
@@ -24,11 +24,15 @@ void readFallingEdge(void);
 
 uint16_t startLowCnts = 0;
 uint16_t startHighCnts = 0;
-uint16_t lowCnts = 0;
-uint16_t oneACnts = 0;
-uint16_t oneBCnts = 0;
+uint32_t lowCnts = 0;
+uint32_t oneACnts = 0;
+uint32_t oneBCnts = 0;
 uint16_t endLowCnts = 0;
 uint8_t numEdges = 0;
+
+uint16_t tmrCounts[100];
+uint16_t training[71];
+bool doneTesting = false; 
 
 bool collectingData = false; 
 
@@ -115,22 +119,35 @@ void main (void) {
             case 't':
                 printf("Press any remote button.\r\n");
                 
-                collectingData = true;
-                while (collectingData == true);
+//                collectingData = true;
+//                while (collectingData == true);
+//                
+//                if (oneACnts > oneBCnts) {
+//                    oneCnts = oneACnts;
+//                    zeroCnts = oneBCnts;
+//                } else {
+//                    oneCnts = oneBCnts;
+//                    zeroCnts = oneACnts;
+//                }
                 
-                if (oneACnts > oneBCnts) {
-                    oneCnts = oneACnts;
-                    zeroCnts = oneBCnts;
-                } else {
-                    oneCnts = oneBCnts;
-                    zeroCnts = oneACnts;
-                }
+                doneTesting = false;
+                while (doneTesting == false);
                   
-                printf("Start: 		Lo: %u uS     Hi: %u uS\r\n", startLowCnts/CONV, startHighCnts/CONV);
-                printf("Data 1:		Lo: %u uS     Hi: %u uS\r\n", lowCnts/CONV, oneCnts/CONV);
-                printf("Data 0:		Lo: %u uS     Hi: %u uS\r\n", lowCnts/CONV, zeroCnts/CONV);
-                printf("Stop:		Lo: %u uS\r\n", endLowCnts/CONV);
-                printf("Half bits: %u half bits per button\r\n", numEdges);
+//                printf("Start: 		Lo: %u uS     Hi: %u uS\r\n", startLowCnts/CONV, startHighCnts/CONV);
+//                printf("Data 1:		Lo: %u uS     Hi: %u uS\r\n", lowCnts/CONV, oneCnts/CONV);
+//                printf("Data 0:		Lo: %u uS     Hi: %u uS\r\n", lowCnts/CONV, zeroCnts/CONV);
+//                printf("Stop:		Lo: %u uS\r\n", endLowCnts/CONV);
+//                printf("Half bits: %u half bits per button\r\n", numEdges);
+                
+                printf("\r\nTimer counts array\r\n");;
+                
+                for (uint8_t i = 0; i < 71; i++) {
+                    printf(" %u ", training[i]);
+                    if (i%8 == 0) {
+                        printf("\r\n");
+                    }
+                    training[i] = 0;
+                }                               
                            
                 break;
 
@@ -188,138 +205,39 @@ void ECCP3_CaptureISR(void) {
     //printf("inISR\r\n");
     static state isrState = START_START_LOW; 
     static uint16_t previousTMRcnts = 0;
-    static uint8_t numDataBits = 0;     // We increment numDataBits every time we process the highCnts
-    static uint8_t numABits = 0;
-    static uint8_t numBBits = 0; 
+    static bool rise = true; 
     
     uint16_t currentTMRcnts = 0;    
     uint16_t checkCnts = 0;
     uint8_t allowance = 5;     // 20% allowance
     
-    currentTMRcnts = CCPR3H;    // Every time ISR triggers, we load the current tmr cnts into this variable
-    currentTMRcnts = (currentTMRcnts << 8) + CCPR3L;
-    
-    numEdges++; 
-    
-    switch (isrState){
-        
-        case START_START_LOW:
-            
-            //printf("inISR START_START_LOW\r\n");
-            //numEdges = 0;
-            numDataBits = 0;
-            numBBits = 0;
-            numABits = 0;
-            
-            previousTMRcnts = currentTMRcnts; 
-            
-            
-            PIR4bits.CCP3IF = 0;
-            if(collectingData == true){
-                isrState = END_START_LOW;
-                readRisingEdge();
-            }
-            break;
-                
-        case END_START_LOW:
-            
-            startLowCnts = currentTMRcnts - previousTMRcnts;
-            
-            previousTMRcnts = currentTMRcnts;
-            
-            readFallingEdge();
-            PIR4bits.CCP3IF = 0;
-            isrState = END_START_HIGH;
-            
-            break;
-            
-        case END_START_HIGH:
-            
-            startHighCnts = currentTMRcnts - previousTMRcnts;
-            
-            previousTMRcnts = currentTMRcnts;
-            
-            readRisingEdge();
-            PIR4bits.CCP3IF = 0;
-            isrState = FIRST_DATA_LOW;
-            
-            break;
-            
-        case FIRST_DATA_LOW:
-            
-            lowCnts = currentTMRcnts - previousTMRcnts;
-            
-            previousTMRcnts = currentTMRcnts;
-            
-            readFallingEdge();
-            PIR4bits.CCP3IF = 0;
-            isrState = FIRST_DATA_HIGH;
-            
-            break;
-            
-        case FIRST_DATA_HIGH:
-            
-            oneACnts = currentTMRcnts - previousTMRcnts; // oneACnts is how long the first data bit is high
-            numABits++; 
-            numDataBits++; 
-            
-            previousTMRcnts = currentTMRcnts;
-            
-            readRisingEdge();
-            PIR4bits.CCP3IF = 0;
-            isrState = DATA_LOW;
-            
-            break;
-            
-        case DATA_LOW:
-            
-            checkCnts = currentTMRcnts - previousTMRcnts;
-            
-            // If checkCnts is 20% more than lowCnts OR if numDataBits == 32, we are at the stop low bit
-            if(checkCnts > (lowCnts + lowCnts*2/10) || numDataBits == 32){ 
-                    endLowCnts = checkCnts; 
-                    isrState = START_START_LOW;
-                    //printf("Data collect false\r\n");
-                    collectingData = false;
-            }else{
-                lowCnts = (lowCnts*numDataBits + checkCnts) / (numDataBits + 1); 
-                
-                isrState = DATA_HIGH;
-            }
-            
-            previousTMRcnts = currentTMRcnts;
-            
-            readFallingEdge();
-            PIR4bits.CCP3IF = 0;
+    if (doneTesting == false) {
+        currentTMRcnts = CCPR3H;    // Every time ISR triggers, we load the current tmr cnts into this variable
+        currentTMRcnts = (currentTMRcnts << 8) + CCPR3L;
 
-            break;
-            
-        case DATA_HIGH:
-            
-            checkCnts = currentTMRcnts - previousTMRcnts;
-            numDataBits++; 
-            
-            // If checkCnts is more than 20 % away from oneACnts, we consider it B high
-            if(checkCnts > (oneACnts + oneACnts / allowance)  || checkCnts < (oneACnts - oneACnts / allowance) ){
-                oneBCnts = (oneBCnts*numBBits + checkCnts) / (numBBits + 1); 
-                numBBits++; 
-            }
-            
-            // Otherwise, it is A high
-            else{
-                oneACnts = (oneACnts*numABits + checkCnts) / (numABits + 1); 
-                numABits++;
-            }
-            
-            isrState = DATA_LOW;
-            
-            previousTMRcnts = currentTMRcnts;
-            
+        training[numEdges] = (currentTMRcnts - previousTMRcnts)/16; 
+
+        numEdges++; 
+        previousTMRcnts = currentTMRcnts; 
+
+        if (rise == true) {
             readRisingEdge();
-            PIR4bits.CCP3IF = 0;
-            
-            break;
+            rise = false;
+        } else {
+            readFallingEdge();
+            rise = true;
+        }
+
+        if (numEdges == 71) {
+            doneTesting = true;
+            rise = true;
+            numEdges = 0;
+            readFallingEdge();
+        }
+
     }
+    
+    PIR4bits.CCP3IF = 0;
 }
 
 void readRisingEdge(void) {
@@ -335,3 +253,122 @@ void readFallingEdge(void) {
 }
 
 
+//switch (isrState){
+//        
+//        case START_START_LOW:
+//            stateCheck[numEdges-1] = 0;
+//            //printf("inISR START_START_LOW\r\n");
+//            //numEdges = 0;
+//            numDataBits = 0;
+//            numBBits = 0;
+//            numABits = 0;
+//            
+//            previousTMRcnts = currentTMRcnts; 
+//            
+//            PIR4bits.CCP3IF = 0;
+//            if(collectingData == true){
+//                isrState = END_START_LOW;
+//                readRisingEdge();
+//                numEdges = 1;
+//            }
+//            break;
+//                
+//        case END_START_LOW:
+//            stateCheck[numEdges-1] = 1;
+//            startLowCnts = currentTMRcnts - previousTMRcnts;
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readFallingEdge();
+//            PIR4bits.CCP3IF = 0;
+//            isrState = END_START_HIGH;
+//            
+//            break;
+//            
+//        case END_START_HIGH:
+//            
+//            startHighCnts = currentTMRcnts - previousTMRcnts;
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readRisingEdge();
+//            PIR4bits.CCP3IF = 0;
+//            isrState = FIRST_DATA_LOW;
+//            
+//            break;
+//            
+//        case FIRST_DATA_LOW:
+//            
+//            lowCnts = currentTMRcnts - previousTMRcnts;
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readFallingEdge();
+//            PIR4bits.CCP3IF = 0;
+//            isrState = FIRST_DATA_HIGH;
+//            
+//            break;
+//            
+//        case FIRST_DATA_HIGH:
+//            
+//            oneACnts = currentTMRcnts - previousTMRcnts; // oneACnts is how long the first data bit is high
+//            numABits++; 
+//            numDataBits++; 
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readRisingEdge();
+//            PIR4bits.CCP3IF = 0;
+//            isrState = DATA_LOW;
+//            
+//            break;
+//            
+//        case DATA_LOW:
+//            
+//            checkCnts = currentTMRcnts - previousTMRcnts;
+//            
+//            // If checkCnts is 20% more than lowCnts OR if numDataBits == 32, we are at the stop low bit
+//            if(checkCnts > (lowCnts + lowCnts*2/10) || numDataBits == 45){ 
+//                    endLowCnts = checkCnts; 
+//                    isrState = START_START_LOW;
+//                    //printf("Data collect false\r\n");
+//                    collectingData = false;
+//            }else{
+//                lowCnts = (lowCnts*numDataBits + checkCnts) / (numDataBits + 1); 
+//                
+//                isrState = DATA_HIGH;
+//            }
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readFallingEdge();
+//            PIR4bits.CCP3IF = 0;
+//
+//            break;
+//            
+//        case DATA_HIGH:
+//            
+//            checkCnts = currentTMRcnts - previousTMRcnts;
+//            numDataBits++; 
+//            
+//            // If checkCnts is more than 20 % away from oneACnts, we consider it B high
+//            if(checkCnts > (oneACnts + oneACnts / allowance)  || checkCnts < (oneACnts - oneACnts / allowance) ){
+//                oneBCnts = (oneBCnts*numBBits + checkCnts) / (numBBits + 1); 
+//                numBBits++; 
+//            }
+//            
+//            // Otherwise, it is A high
+//            else{
+//                oneACnts = (oneACnts*numABits + checkCnts) / (numABits + 1); 
+//                numABits++;
+//            }
+//            
+//            isrState = DATA_LOW;
+//            
+//            previousTMRcnts = currentTMRcnts;
+//            
+//            readRisingEdge();
+//            PIR4bits.CCP3IF = 0;
+//            
+//            break;
+//    }
